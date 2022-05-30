9b27d9ed890d3a62f9f8ad6e56b6a4ab0ae9d2d3
JonathanRazakalalaina <jonathanbill17@gmail.com>
2022-05-30 17:36:52 +0300

    Ajout de nouvelles lignes de codes

--- script.js
+++ script.js
@@ -173,17 +173,269 @@
 
 // ==============================================================
 
-let N, PG, IPG, i;
-PG = 0;
+// let N, PG, IPG, i, result;
+// PG = 0;
+// result = [];
+// function error() {
+//     alert('Impossible de continuer vous n\'avez pas entré un nombre !');
+// }
+// for(i = 1; i <= 4; i++) {
+//     N = parseInt(prompt('Entrez un nombre'));
+//     if(isNaN(N)) {
+//         error();
+//         break;
+//     };
+//     if (i == 1 || N > PG) {
+//         PG = N;
+//         IPG = i;
+//     };
+//     result.push(N);
+// };
+// alert(`Vous avez entré ${result.join(' - ')}\nParmis eux le plus grand nombre est ${PG}. \nIl a été saisie en position n°${IPG} !`);
 
-for(i = 1; i <= 4; i++) {
-    N = parseInt(prompt('Entrez un nombre'));
-    if (i == 1 || N > PG) {
-        PG = N;
-        IPG = i;
-    };
-};
-alert(`Le plus grand nombre que vous avez entré est ${PG}, c'était au ${IPG}ème saisie !`);
+// ==============================================================
+
+// let N, PG, IPG, i, result;
+// PG = 0;
+// i = 0;
+// result = [];
+
+// do {
+//     N = parseInt(prompt('Entrez un nombre (appuyer sur ESPACE pour arrêter) :'));
+//     i++;
+//     if(i == 1 || N > PG ) {
+//         PG = N;
+//         IPG = i;
+//     };
+//     result.push(N);
+// } while (!isNaN(N));
+// alert(`Vous avez entré : ${result.join(' - ')}\nParmis eux le plus grand nombre est ${PG}. \nIl a été saisie en position n°${IPG} !`);
+
+// ==============================================================
+
+// function transaction(){
+//     let N1,N2, somdue, reste, NB10E, NB5E, result;
+//     N1 = 1;
+//     somdue = 0;
+//     result = [];
+//     do{
+//         N1 = parseInt(prompt('Entrez le montant d\'un produit (entrez 0 s\'il y en a plus):'));
+//         somdue += N1;
+//         result.push(N1);
+//     } while (N1 != 0);
+//     alert('Maintenant entrez la somme de votre argent !')
+
+//     N2 = parseInt(prompt('Vous avez versé :'))
+//     reste = N2 - somdue;
+
+
+//     if(reste < 0){
+//         return alert('Votre argent est insuffisant pour l\'achat que vous tentez d\'effectuer');
+//     };
+
+//     alert(`La somme que nous devons vous rendre est: ${reste} €`);
+
+//     NB10E = 0;
+//     while (reste >= 10) {
+//         NB10E++;
+//         reste -= 10;
+//     }
+//     NB5E = 0;
+//     if (reste >= 5) {
+//         NB5E = 1;
+//         reste -= 5;
+//     }
+
+//     alert(`La somme de votre achat est: ${somdue} € \nEn détail c'était: \n${result.join(' €\n')} \nVous avez versé : ${N2} € \nDétail de la rendue : \n${NB10E} billet(s) de 10 € \n${NB5E} billet de 5 € \n${reste} pièce(s) de 1 €`)
+// }
+// transaction();
+
+
+// function transaction(){
+//     let N1, N2, somdue, reste, NB5000A, NB2000A, NB1000A, NB500A, NB200A, NB100A, result, resteF;
+//     N1 = 1;
+//     somdue = 0;
+//     result = [];
+//     do{
+//         N1 = parseInt(prompt(`Entrez le prix en AR d\'un produit que vous avez acheté (entrez 0 s\'il y en a plus) - ${somdue} AR:`));
+//         somdue += N1;
+//         result.push(N1);
+//     } while (N1 != 0);
+
+//     alert(`La somme à payer pour votre achat est ${somdue} AR \nMaintenant entrez la somme de votre argent !`);
+
+//     N2 = parseInt(prompt(`Vous devez payer ${somdue} AR \nSomme de votre argent :`));
+//     reste = N2 - somdue;
+
+
+//     if(reste < 0){
+//         return alert(`Votre argent est insuffisant pour l\'achat que vous tentez d\'effectuer \nDétail: \nSomme versée: ${N2} AR < Total à payer: ${somdue} AR`);
+//     };
+
+//     alert(`La somme que nous devons vous rendre est: ${reste} AR`);
+
+//     NB5000A = 0;
+//     while (reste >= 5000) {
+//         NB5000A++;
+//         reste -= 5000;
+//     }
+//     NB2000A = 0;
+//     while (reste >= 2000 && reste < 5000) {
+//         NB2000A++;
+//         reste -= 2000;
+//     }
+//     NB1000A = 0;
+//     while (reste >= 1000 && reste < 2000) {
+//         NB1000A++;
+//         reste -= 1000;
+//     }
+//     NB500A = 0;
+//     while (reste >= 500 && reste < 1000) {
+//         NB500A++;
+//         reste -= 500;
+//     }
+
+//     NB200A = 0;
+//     while(reste >= 200 && reste < 500) {
+//         NB200A++;
+//         reste -= 200;
+//     }
+
+//     NB100A = 0;
+//     if (reste >= 100 && reste < 200) {
+//         NB100A = 1;
+//         reste -= 100;
+//     }
+//     resteF = NB5000A*5000 + NB2000A*2000 + NB1000A*1000 + NB500A*500 + NB200A*200 + NB100A*100
+
+//     alert(`============= RESUME DE VOTRE ACHAT ============= \n${result.join(' AR\n')} \n\nTOTALE A PAYER : ${somdue} AR \nTOTALE VERSEE : ${N2} AR \nA RENDRE : ${resteF} AR \n\nDétail de la rendue : \n${NB5000A} billet(s) de 5000 AR \n${NB2000A} billet(s) de 2000 AR \n${NB1000A} billet(s) de 1000 AR \n${NB500A} billet(s) de 500 AR \n${NB200A} billet(s) de 200 AR \n${NB100A} billet de 100 AR`);
+//     alert('Merci de votre visite. A bientôt !')
+// }
+// transaction();
+
+// ==============================================================
+
+// let mois;
+// const saison1 = 'ÉTÉ';
+// const saison2 = 'PRINTEMPS';
+// const saison3 = 'HIVER';
+
+// function numbmonth() {
+//     mois = parseInt(prompt('Afin de connaître les différentes saisons du temps à Madagascar, \nEntrez un numéro de mois compris entre 1 et 12:'));
+// }
+// numbmonth();
+
+// while(mois < 1 || mois > 12) {
+//     alert(`${mois} ?? Ce numéro n'existe pas ! Recommencez la saisie`);
+//     numbmonth();
+// };
+
+// switch (mois) {
+//     case 1:
+//         alert(`Nous sommes en Janvier c'est l'${saison1} !`);
+//         break;
+
+//     case 2:
+//         alert(`Nous sommes en Février c'est l'${saison1} !`);
+//         break;
+
+//     case 3:
+//         alert(`Nous sommes en Mars l'${saison1} prend fin et le ${saison2} commence !`);
+//         break;
+
+//     case 4:
+//         alert(`Nous sommes en Avril c'est le ${saison2} !`);
+//         break;
+
+//     case 5:
+//         alert(`Nous sommes en Mai le ${saison2} prend fin et l'${saison3} commence !`);
+//         break;
+
+//     case 6:
+//         alert(`Nous sommes en Juin c'est l'${saison3} !`);
+//         break;
+
+//     case 7:
+//         alert(`Nous sommes en Juillet c'est l'${saison3} !`);
+//         break;
+
+//     case 8:
+//         alert(`Nous sommes en Aoùt c'est l'${saison3} !`);
+//         break;
+
+//     case 9:
+//         alert(`Nous sommes en Septembre l'${saison3} prend fin et l'${saison1} commence !`);
+//         break;
+
+//     case 10:
+//         alert(`Nous sommes en Octobre c'est l'${saison1} !`);
+//         break;
+
+//     case 11:
+//         alert(`Nous sommes en Novembre c'est l'${saison1} !`);
+//         break;
+
+//     case 12:
+//         alert(`Nous sommes en Décembre c'est l'${saison1} !`);
+//         break;
+// }
+
+
+// let x, y;
+// x = 4;
+// y = 3;
+
+// console.log(x%y);
+
+// let a, b, q, r;
+
+// a = 10;
+// b = 3;
+// q = 1;
+// r = a;
+
+// while(r >= b) {
+//     r %= b;
+//     q++;
+// }
+// console.log(`Le quotient ${q} et le reste ${r}`);
+
+// ==============================================================
+
+let caractere;
+let NBA;
+function saisie() {
+    caractere = prompt('Entrez une lettre (appuyer sur " * " pour arrêter)');
+}
+NBA = 0;
+do {
+    saisie();
+    if(caractere === 'a') {
+        NBA += 1;
+    }
+} while (caractere != '*');
+
+alert(`La lettre "a" est apparue ${NBA} fois !`);
+
+// ==============================================================
+
+// let x, r, p, bin, result;
+// x = prompt('Entrez un nombre :'); 
+// console.log(typeof(x));
+// p = 1;
+// bin = 0;
+// result = [];
+
+// do {
+//     r = x%2;
+//     bin += (r*p);
+//     p *= 10;
+//     x /= 2;
+//     parseInt(x);
+//     result.push(bin);
+// } while (x != 0);
+// z = typeof(bin)
+// alert(result.join(''));
 
 
 
